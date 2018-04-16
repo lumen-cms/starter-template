5d55991978ed5122f37c05ee6d0c4ba075977e77
Steffen Baer <stbaer.mail@gmail.com>
2018-04-16 11:51:32 +0800

    updated eslint config, removed graphql-request

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -10,12 +10,33 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/recommended', 'standard'
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue', 'graphql'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    "prefer-const": ["error", {
+      "destructuring": "any",
+      "ignoreReadBeforeAssign": false
+    }],
+    "indent": "off",
+    "indent-legacy": ["error", 2],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 4,
+      'multiline': {
+        'max': 4,
+        'allowFirstLine': true
+      }
+    }],
+    'vue/order-in-components': 0,
+    'vue/require-default-prop': 0,
+    'vue/attributes-order': 0
+  }
 }
